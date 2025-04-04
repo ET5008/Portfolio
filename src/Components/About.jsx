4e8b7f8f39f1e248311e0c8f52c26f4eb4593016
Ethan Tran <tran.ethan.000@gmail.com>
2025-04-04 15:47:04 -0700

    Fixed Scrolling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,8 +2,8 @@
 
 export default function About() {
     return(
-        <section className="mt-24 flex flex-col items-center mb-36">
-            <h2 id="About" className="text-3xl">About Me</h2>
+        <section id="About" className="mt-24 flex flex-col items-center mb-36">
+            <h2 className="text-3xl">About Me</h2>
             <p className="text-xl w-[30vw] text-center mt-4">I'm an incoming freshman at UC Berkeley, majoring in Computer Science. I enjoy developing passion projects in the form of website development. Save this website and watch my journey real time!</p>
         </section>
     )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,21 @@
 
 export default function Header(){
+
+    const handleClickNav = (section) => {
+        document.getElementById(section).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        })
+    }
+
     return (
         <header className='flex flex-row items-center justify-around w-[100vw] bg-slate-500/95 py-6 fixed'>
             <h1 className="text-4xl font-medium">ET</h1>
             <nav className="items-center">
                 <ul className="flex flex-row align-middle gap-4">
-                    <li className='text-lg hover:border-b-[2px] transition-all duration-150'> <a href="#About">About Me</a></li>
-                    <li className='text-lg hover:border-b-[2px] transition-all duration-150'> <a href="#Projects">Projects</a></li>
-                    <li className='text-lg hover:border-b-[2px] transition-all duration-150'> <a href="#Contact">Contact</a></li>
+                    <li className='text-xl hover:text-blue-400  transition-all duration-150'> <button className="" onClick={() => handleClickNav('About')} >About Me</button></li>
+                    <li className='text-xl hover:text-blue-400  transition-all duration-150'> <button className="" onClick={() => handleClickNav('Projects')} >Projects</button></li>
+                    <li className='text-xl hover:text-blue-400  transition-all duration-150'> <button className="" onClick={() => handleClickNav('Contact')} >Contact</button></li>
                 </ul>
             </nav>
         </header>
